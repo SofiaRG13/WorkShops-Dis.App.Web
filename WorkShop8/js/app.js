b385aa13c0bd792e80b98dc3c275c27e0ca3ab25
Sofía Rodríguez <sofirodriguez.356@gmail.com>
2021-11-22 14:08:53 -0600

    WorkShop 8

---
+++ WorkShop8/js/app.js
@@ -0,0 +1,67 @@
+function addBook() {
+  //read the book title field
+  const bookName = document.getElementById('title').value;
+
+  console.log('El libro es:', bookName);
+  //insert to a database
+  let booksDb = JSON.parse(localStorage.getItem('books'));
+  if(!booksDb) {
+    booksDb = [];
+  }
+  booksDb.push(bookName);
+  localStorage.setItem('books', JSON.stringify(booksDb));
+  //reload the book list
+  showListOfBooks();
+  llenarTabla();
+}
+
+function showListOfBooks(){
+  const bookName = localStorage.getItem('books');
+
+ 
+  // read books from localstorage
+  // generate the HTML table to show the book
+}
+
+
+function validateTitle() {
+  //read the book title field
+  const bookName = document.getElementById('title').value;
+  if(bookName.length > 3) {
+    document.getElementById('add-book-button').disabled = false;
+  } else {
+    document.getElementById('add-book-button').disabled = true;
+  }
+}
+
+
+function validateBooks(){
+  let booksDb = JSON.parse(localStorage.getItem('books'));
+  if(booksDb!=null) {
+    llenarTabla();
+  }
+}
+
+function llenarTabla() {
+  var tbody=document.querySelector('#table-light tbody');
+
+  tbody.innerHTML='';
+
+  var book= JSON.parse(localStorage.getItem('books'));
+
+  var ncantidad=book.length;
+
+  for(var i=0; i<ncantidad; i++){
+    var fila= document.createElement('tr');
+
+    var celdaTitulo= document.createElement('td');
+
+    var titulo= document.createTextNode(book[i]);
+
+    fila.appendChild(celdaTitulo.appendChild(titulo));
+
+    tbody.appendChild(fila);
+  }
+}
+
+showListOfBooks();
